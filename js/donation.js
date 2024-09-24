f95e0259dd27f0c387ab679089b1b3aed0637e62
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2024-09-24 09:36:44 +0600

    finnal commit adding live date and time

--- js/donation.js
+++ js/donation.js
@@ -12,16 +12,16 @@ document.getElementById("noakhali-btn").addEventListener("click", function () {
     document.getElementById("my_modal_1").showModal();
 
     // add to transaction history
+    const dateTime = Date();
     const entry = document.createElement("p");
-    entry.innerHTML = `<h1 class=" mx-auto mt-5 text-1xl px-5 py-5 font-bold border-2 rounded-lg">
-          ${noakhaliInput}  Taka is Donated for Flood at Noakhali, Bangladesh        
-        </h1> `;
+    entry.innerHTML = `<div class="mx-auto mt-5 border-2 rounded-lg">
+      <h1 class="text-2xl px-5 py-5 font-bold">
+       ${noakhaliInput} Taka is Donated for Flood at Noakhali, Bangladesh
+      </h1>
+      <p class="px-5 pb-5">${dateTime}</p>
+      </div>`;
     document.getElementById("history-container").appendChild(entry);
-    
-   
-  }
-  
-  else {
+  } else {
     alert("something is wrong");
   }
 });
@@ -38,11 +38,15 @@ document.getElementById("feni-btn").addEventListener("click", function () {
     document.getElementById("feni-balance").innerText = noakhalitotal;
     document.getElementById("my_modal_1").showModal();
     // add to transaction history
+    const dateTime = Date();
     const entry = document.createElement("p");
-    entry.innerHTML = `<h1 class=" mx-auto mt-5 text-1xl px-5 py-5 font-bold border-2 rounded-lg">
-          ${noakhaliInput}  Taka is Donated for Flood Relief in Feni, Bangladesh        
-        </h1> `;
-    document.getElementById("history-container").appendChild(entry);
+    entry.innerHTML = `<div class="mx-auto mt-5 border-2 rounded-lg">
+      <h1 class="text-2xl px-5 py-5 font-bold">
+       ${noakhaliInput} Taka is Donated for  Flood Relief in Feni, Bangladesh
+      </h1>
+      <p class="px-5 pb-5">${dateTime}</p>
+      </div>`;
+      document.getElementById("history-container").appendChild(entry);
   } else {
     alert("something is wrong");
   }
@@ -60,14 +64,15 @@ document.getElementById("aid-btn").addEventListener("click", function () {
     document.getElementById("aid-balance").innerText = noakhalitotal;
     document.getElementById("my_modal_1").showModal();
     // add to transaction history
-
+    const dateTime = Date();
     const entry = document.createElement("p");
-    entry.innerHTML = `<h1 class=" mx-auto mt-5 text-1xl px-5 py-5 font-bold border-2 rounded-lg">
-          ${noakhaliInput}  Taka is Donated for Injured in the Quota Movement        
-        </h1> `;
-    document.getElementById("history-container").appendChild(entry);
-    
-    
+    entry.innerHTML = `<div class="mx-auto mt-5 border-2 rounded-lg">
+      <h1 class="text-2xl px-5 py-5 font-bold">
+       ${noakhaliInput} Taka is Donated for Injured in the Quota Movement, Bangladesh
+      </h1>
+      <p class="px-5 pb-5">${dateTime}</p>
+      </div>`;
+      document.getElementById("history-container").appendChild(entry);
   } else {
     alert("something is wrong");
   }
